c00254c03ade5e92994c81695f58b4a003cb863f
paulostevo <41132104+paulostevo@users.noreply.github.com>
2023-05-13 21:50:35 -0300

    Add project files.

---
+++ GerenciadorNegocios/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using GerenciadorNegocios.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GerenciadorNegocios.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
